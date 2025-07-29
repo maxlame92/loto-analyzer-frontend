a3389fcbe5b6b8ee1b23436a2eb2b611bd7c2211
maxlame92 <aboucho92@gmail.com>
2025-07-29 23:45:46 +0200

    dvbn

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import './App.css';
 
-const API_URL = 'http://localhost:8000';
+const API_URL = 'https://analyzer-oxxt.onrender.com';
 
 function App() {
   const [email, setEmail] = useState('');
